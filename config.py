02cd47e8922fbc8977e12419d3079c71bd0b2bda
Yantizz98 <128052178+Yantizz98@users.noreply.github.com>
2023-05-13 14:03:58 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+TOKEN = "6114493557:AAEeLjD2ntXeBeMnHB54vcYo3CrOiwMS2vc"
+keys = {
+    'Dollar': 'USD',
+    'Euro': 'EUR',
+    'Ruble': 'RUB'
+}
+
+# keys = {
+#      'USD': 'Доллар',
+#      'EUR': 'Евро',
+#      'RUB': 'Рубль'}
+
+# keys = {
+#      'Доллар': 'USD',
+#      'Евро': 'EUR',
+#      'Рубль': 'RUB'}
+
---
+++ extensions.py
@@ -0,0 +1,45 @@
+import json
+import requests
+from config import keys
+
+
+class APIException(Exception):
+    pass
+
+
+class Convertor:
+    @staticmethod
+    def get_price(base, quote, amount):
+        try:
+            base_key = keys[base.lower()]
+        except KeyError:
+            raise APIException(f"Currency {base} not found!")
+
+        try:
+            quote_key = keys[quote.lower()]
+        except KeyError:
+            raise APIException(f"Currency {quote} not found!")
+
+        if base_key == quote_key:
+            raise APIException(f'Not possible to transfer identical currencies {base}!')
+
+        try:
+            amount = float(amount.replace(",", "."))
+        except ValueError:
+            raise APIException(f'Failed to process quantity {amount}!')
+
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={quote_key}&from={base_key}&amount={amount}"
+        payload = {}
+        headers = {"apikey": "PpXybTjFDFwPJQbtgtNEzznBDeYkohf8"}
+        response = requests.get(url, headers=headers, data=payload)
+        total_base = json.loads(response.content)
+        # print(total_base)
+        # # r = requests.get(f"https://api.apilayer.com/exchangerates_data/live?base={base}&symbols={sym}")
+        # # resp = json.loads(r.content)
+        new_price = total_base * amount
+        new_price = round(new_price, 3)
+        message = f"Price {amount} {base} in {quote} : {new_price}"
+        return message
+
+
+
